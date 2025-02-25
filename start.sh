c1eeb471136122a1262b2befc534d8d65a0917a9
Nicole Birova <nicolebirova2024@u.northwestern.edu>
2025-02-25 12:28:17 -0600

    start.sh updates

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT &
-streamlit run ui.py
+uvicorn main:app --host 0.0.0.0 --port 8000 &  # Start FastAPI in the background
+streamlit run ui.py --server.port 8501 --server.address 0.0.0.0  # Start Streamlit
